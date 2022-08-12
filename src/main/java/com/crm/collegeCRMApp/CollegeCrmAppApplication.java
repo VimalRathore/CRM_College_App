84f45ac94c3aa5f1bf3abbaedd59b10d45de1bb2
vimal.singh <vimal.singh@servicenow.com>
2022-08-12 20:16:33 +0530

    firstcommit

---
+++ src/main/java/com/crm/collegeCRMApp/CollegeCrmAppApplication.java
@@ -0,0 +1,13 @@
+package com.crm.collegeCRMApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeCrmAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeCrmAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crm/collegeCRMApp/CollegeCrmAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crm.collegeCRMApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeCrmAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
